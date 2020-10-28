9c3e1302ffd63125ced2c5f313a659843d6f9746
goszpeti <43680650+goszpeti@users.noreply.github.com>
2020-10-28 19:34:51 +0100

    Fix issue with no arguments of cmd line executables in Windows

--- src/conan_app_launcher/app_executor.py
+++ src/conan_app_launcher/app_executor.py
@@ -5,22 +5,25 @@ from conan_app_launcher.config_file import AppEntry
 
 
 def execute_app(app_info: AppEntry):
+    """ Executes an application with args and optionally spawns a new shell as specified in the app entry."""
     if app_info.executable.absolute().is_file():
+        cmd = [str(app_info.executable)]
         # Linux call errors on creationflags argument, so the calls must be separated
         if platform.system() == "Windows":
             creationflags = 0
             if app_info.is_console_application:
                 creationflags = subprocess.CREATE_NEW_CONSOLE
-            subprocess.Popen(executable=str(app_info.executable),
-                             args=app_info.args.strip().split(" "), creationflags=creationflags)
+            if app_info.args:
+                cmd += app_info.args.strip().split(" ")
+                # don't use 'executable' arg of Popen, because then shell scripts won't execute correctly
+            subprocess.Popen(cmd, creationflags=creationflags)
         elif platform.system() == "Linux":
             if app_info.is_console_application:
                 # Sadly, there is no default way to do this, because of the miriad terminal emulators available
-                # Use the default distro emulator, with x-terminal-emulator (sudo update-alternatives --config x-terminal-emulator)
+                # Use the default distro emulator, with x-terminal-emulator
+                # (sudo update-alternatives --config x-terminal-emulator)
                 # This works only on debian distros.
                 cmd = ["x-terminal-emulator", "-e", str(app_info.executable)]
-            else:
-                cmd = [str(app_info.executable)]
             if app_info.args:
                 cmd += app_info.args.strip().split(" ")
             subprocess.Popen(cmd)
