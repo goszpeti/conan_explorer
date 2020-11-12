d9c252a8f4c482d4dad39e6bbdbdca393ee34829
peter-gosztolya <peter.gosztolya@gmail.com>
2020-11-13 00:10:11 +0100

    Fix test evaluation

--- test/evaluation.py
+++ test/evaluation.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 import xunitparser
 
+test_error = False
 for result_file in Path(".").glob("result*.xml"):
     ts, tr = xunitparser.parse(open(str(result_file)))
-    if tr.wasSuccessful():
-        sys.exit(0)
-    else:
+    if not tr.wasSuccessful():
         sys.exit(1)
+sys.exit(0)
