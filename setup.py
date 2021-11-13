9159c724102ff265e3b06670290586b594e96c24
peter-gosztolya <peter.gosztolya@gmail.com>
2021-11-13 20:15:38 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Note: To use the 'upload' functionality of this file, you must:
+# Note: To use the "upload" functionality of this file, you must:
 #   $ pipenv install twine --dev
 
 import io
@@ -12,11 +12,12 @@ from os.path import basename, splitext
 from setuptools import find_packages, setup
 
 # Package meta-data.
-NAME = 'conan-app-launcher'
-DESCRIPTION = 'App Launcher and Package Explorer for Conan'
-URL = 'https://github.com/goszpeti/conan_app_launcher'
-AUTHOR = 'Peter Gosztolya'
-REQUIRES_PYTHON = '>=3.6.0'
+NAME = "conan-app-launcher"
+VERSION = "1.0.0b9"
+DESCRIPTION = "App Launcher and Package Explorer for Conan"
+URL = "https://github.com/goszpeti/conan_app_launcher"
+AUTHOR = "Péter Gosztolya and Contributors"
+REQUIRES_PYTHON = ">=3.6.0"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
@@ -24,16 +25,17 @@ REQUIRED = [
     "conan>=1.24",  # MIT License
     "jsonschema>=3.2.0",  # MIT License
     "pefile==2021.9.3",  # MIT License
-    "typing-extensions>=3.10.0.2 ; python_version<'3.8'", # Python Software Foundation License(PSF)
-    "dataclasses>=0.8 ; python_version<'3.7'"  # Apache Software License (Apache)
+    'importlib-metadata>=4.8.2 ; python_version<"3.8"',  # Apache Software License (Apache)
+    'typing-extensions>=3.10.0.2 ; python_version<"3.8"', # Python Software Foundation License(PSF)
+    'dataclasses>=0.8 ; python_version<"3.7"'  # Apache Software License (Apache)
 ]
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Import the README and use it as the long-description.
 try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
     # replace links
     temp = []
     if os.getenv("GITHUB_REF"):
@@ -52,28 +54,23 @@ try:
 except FileNotFoundError:
     long_description = DESCRIPTION
 
-# Load the package's __version__.py module as a dictionary.
-about = {}
-with open(os.path.join(here, "src", "conan_app_launcher", '__version__.py')) as f:
-    exec(f.read(), about)
-
 
 # Where the magic happens:
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=VERSION,
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     python_requires=REQUIRES_PYTHON,
     url=URL,
     packages=find_packages("src"),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     install_requires=REQUIRED,
     include_package_data=True,
-    license='MIT',
+    license="MIT",
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -93,8 +90,8 @@ setup(
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
-        'gui_scripts': [
-            'conan_app_launcher=conan_app_launcher.__main__:main',
+        "gui_scripts": [
+            "conan_app_launcher=conan_app_launcher.__main__:main",
         ]
     },
 )
