c179f56db014d6bccd18b36097545488064693a5
goszpeti <43680650+goszpeti@users.noreply.github.com>
2021-11-16 22:22:37 +0100

    Bump up version to 1.0.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import find_packages, setup
 
 # Package meta-data.
 NAME = "conan-app-launcher"
-VERSION = "1.0.0b9"
+VERSION = "1.0.0"
 DESCRIPTION = "App Launcher and Package Explorer for Conan"
 URL = "https://github.com/goszpeti/conan_app_launcher"
 AUTHOR = "Péter Gosztolya and Contributors"
