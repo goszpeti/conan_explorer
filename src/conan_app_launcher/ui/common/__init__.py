50270b383867c6ba19fcad41fa7d3edac744ff49
peter-gosztolya <peter.gosztolya@gmail.com>
2022-02-15 21:02:59 +0100

    Rework async load and add  wait_for_finished function and worker args

--- src/conan_app_launcher/ui/common/__init__.py
+++ src/conan_app_launcher/ui/common/__init__.py
@@ -1,13 +1,12 @@
 """ Common ui classes, and functions """
+
 from typing import Callable, Optional, Tuple
 
 from conan_app_launcher.logger import Logger
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtCore import Qt
 
-ASYNC_LOADING = True  # For debug purposes: errors in Qt Threads are not debuggable
-
-
+QtCore.QSysInfo
 class Worker(QtCore.QObject):
     """ Generic worker for Qt, which can call any function with args """
     finished = QtCore.pyqtSignal()
@@ -18,19 +17,27 @@ class Worker(QtCore.QObject):
         self.args = args
 
     def work(self):
+        #import debugpy  # - debug with this the Qt Thread
+        #debugpy.debug_this_thread()
         self.func(*self.args)
         self.finished.emit()
 
 
 class QtLoaderObject(QtCore.QObject):
-    def __init__(self, parent) -> None:
+    
+    def __init__(self, parent: QtCore.QObject):
         super().__init__(parent)
         self.progress_dialog: Optional[QtWidgets.QProgressDialog] = None
         self.worker: Optional[Worker] = None
         self.load_thread: Optional[QtCore.QThread] = None
+        self.finished = False
 
-    def async_loading(self, parent, work_task: Callable, finish_task: Optional[Callable], loading_text: str):
-        self.progress_dialog = QtWidgets.QProgressDialog(parent)
+    def async_loading(self, dialog_parent: QtWidgets.QWidget, work_task: Callable, worker_args: Tuple = (),
+                        finish_task: Optional[Callable] = None,
+                        loading_text: str = "Loading"):
+        self.finished = True
+
+        self.progress_dialog = QtWidgets.QProgressDialog(dialog_parent)
         self.progress_dialog.setLabelText(loading_text)
         # Window flags to disable close button
         self.progress_dialog.setWindowFlags(Qt.Window | Qt.WindowTitleHint | Qt.CustomizeWindowHint)
@@ -41,23 +48,30 @@ class QtLoaderObject(QtCore.QObject):
         self.progress_dialog.setMinimumDuration(1000)
         self.progress_dialog.show()
 
-        if ASYNC_LOADING:
-            self.worker = Worker(work_task)
-            self.load_thread = QtCore.QThread()
-            self.worker.moveToThread(self.load_thread)
-            self.load_thread.started.connect(self.worker.work)
+        self.worker = Worker(work_task, worker_args)
+        self.load_thread = QtCore.QThread()
+        self.load_thread.setObjectName(f"loader_thread_{str(self)}")
+        self.worker.moveToThread(self.load_thread)
+        self.load_thread.started.connect(self.worker.work)
 
-            if finish_task:
-                self.worker.finished.connect(finish_task)
-            self.worker.finished.connect(self.worker.deleteLater)
-            self.worker.finished.connect(self.load_thread.quit)
+        if finish_task:
+            self.load_thread.finished.connect(finish_task)
+        self.load_thread.finished.connect(self.thread_finished)
 
-            self.load_thread.finished.connect(self.load_thread.deleteLater)
-            self.load_thread.finished.connect(self.progress_dialog.hide)
-            Logger().debug(f"Start async loading thread for {str(work_task)}")
-            self.load_thread.start()
-        else:
-            work_task()
-            if finish_task:
-                finish_task()
-                self.progress_dialog.hide()
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.worker.finished.connect(self.load_thread.quit)
+
+        self.load_thread.finished.connect(self.load_thread.deleteLater)
+        self.load_thread.finished.connect(self.progress_dialog.hide)
+        Logger().debug(f"Start async loading thread for {str(work_task)}")
+        self.load_thread.start()
+
+    def thread_finished(self):
+        self.finished = True
+
+    def wait_for_finished(self): # TODO timeout + cancel
+        Logger().debug("wait for loading thread")
+        # execute once
+        while not self.finished:
+            QtWidgets.QApplication.processEvents()
+
