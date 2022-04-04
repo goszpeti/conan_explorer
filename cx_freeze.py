494b3f3c5c4f77899ea03ca34db6bf6398ca400f
peter-gosztolya <peter.gosztolya@gmail.com>
2022-04-04 21:36:30 +0200

    Add cxfreeze file and minor fixes to bug dialog

---
+++ cx_freeze.py
@@ -0,0 +1,97 @@
+"""
+A simple setup script to create an executable using PyQt5. This also
+demonstrates the method for creating a Windows executable that does not have
+an associated console.
+PyQt5app.py is a very simple type of PyQt5 application
+Run the build process by running the command 'python setup.py build'
+If everything works well you should find a subdirectory in the build
+subdirectory that contains the files needed to run the application
+"""
+
+import sys
+from setup import AUTHOR, DESCRIPTION, URL, VERSION
+from cx_Freeze import Executable, setup
+try:
+    from cx_Freeze.hooks import get_qt_plugins_paths
+except ImportError:
+    include_files = []
+else:
+    # Inclusion of extra plugins (new in cx_Freeze 6.8b2)
+    # cx_Freeze imports automatically the following plugins depending of the
+    # use of some modules:
+    # imageformats - QtGui
+    # platforms - QtGui
+    # mediaservice - QtMultimedia
+    # printsupport - QtPrintSupport
+    include_files = get_qt_plugins_paths("PyQt5", "platforms")
+
+# base="Win32GUI" should be used only for Windows GUI app
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+qt5libdir = "lib/PyQt5/Qt5/bin/"
+build_exe_options = {
+    "includes": "conan_app_launcher",
+    "excludes": ["debugpy"],
+    "bin_excludes": ['Qt5dbus.dll', 'Qt5Network.dll', 'Qt5Qml.dll', "Qt5QmlModels.dll",
+                     'Qt5Quick.dll', 'Qt5Svg.dll', 'Qt5WebSockets.dll', "d3dcompiler_47.dll", "opengl32sw.dll",
+    "libicudata.so.66", "libgtk-3.so.0", "libQt5Quick.so.5", "libQt5Qml.so.5", "libicui18n.so.66", "libicui18n.so.66",
+    "libQt5Network.so.5", "libQt5QmlModels.so.5"],
+    "include_files": include_files,
+    "zip_include_packages" : ['PyQt5', "conans"]
+}
+
+icon = "src/conan_app_launcher/assets/icons/icon.ico"
+app_name = "Conan App Launcher"
+
+bdist_mac_options = {
+    "bundle_name": app_name,
+}
+
+bdist_dmg_options = {
+    "volume_label": app_name,
+}
+
+directory_table = [
+   # ("ProgramMenuFolder", "TARGETDIR", "."),
+   # ("Conan App Launcher", "ProgramMenuFolder", "MYPROG~1|My Program"),
+]
+
+msi_data = {
+    "Directory": directory_table,
+    "ProgId": [
+        ("Prog.Id", None, None, "This is a description", "IconId", None),
+    ],
+    "Icon": [
+        ("IconId", icon)
+    ],
+    # "summary_data": {
+    #     "author": "Peter Gosztolya and Contributors",
+    # },
+}
+
+bdist_msi_options = {
+    #"target_name": "conan_app_launcher_setup",
+    "data": msi_data,
+    "install_icon": icon}
+
+executables = [Executable("./src/conan_app_launcher/__main__.py",
+                          base=base, target_name="conan_app_launcher",
+                          icon=icon, shortcut_name=app_name,
+                          shortcut_dir="DesktopFolder",
+                          )]
+
+setup(
+    name=app_name,
+    author=AUTHOR,
+    version=VERSION,
+    description=DESCRIPTION,
+    url=URL,
+    options={
+        "build_exe": build_exe_options,
+        "bdist_mac": bdist_mac_options,
+        "bdist_dmg": bdist_dmg_options,
+        "bdist_msi": bdist_msi_options
+    },
+    executables=executables,
+)
--- src/conan_app_launcher/ui/dialogs/bug_dialog.py
+++ src/conan_app_launcher/ui/dialogs/bug_dialog.py
@@ -8,7 +8,8 @@ import traceback
 
 from conan_app_launcher import REPO_URL, __version__, base_path
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QMessageBox, QSizePolicy, QTextEdit
+from PyQt5.QtGui import QFontMetrics
 
 bug_dialog_text = f"""
 **Describe the bug**
@@ -34,24 +35,45 @@ Add any other context about the problem here.
 """
 
 
+class MyMessageBox(QMessageBox):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self._width = 150   # default
+
+        self.setSizeGripEnabled(True)
+
+    def setWidth(self, width):
+        self._width = width
+
+    def resizeEvent(self, event):
+        _result = super().resizeEvent(event)
+
+        self.setFixedWidth(self._width)
+
+        return _result
+
+
 def bug_reporting_dialog(excvalue, tb):
     import urllib.parse  # late import hopefully we don't need this
     error_text = f"{excvalue}\n" + "\n".join(traceback.format_tb(tb, limit=None))
     title = urllib.parse.quote("Application Crash on <>")
     body = urllib.parse.quote(f"{bug_dialog_text}\n**Stacktrace**:\n" + error_text)
     new_issue_with_info_text = f"{REPO_URL}/issues/new?title={title}&body={body}&labels=bug"
-    html_crash_text = f'<html><head/><body><p> \
+    html_crash_text = f'\
+        <html><head/><body><p> \
         Oops, something went wrong!\
         To help improve the program, please post a <a href="{new_issue_with_info_text}"> \
         <span style=" text-decoration: underline color:  # 0000ff;" \
         >new github issue</span></a> and describe, how the crash occured.'
-    dialog = QMessageBox(None) # TODO style?
+    dialog = MyMessageBox()  # TODO style?
+    #dialog.setStyleSheet("QLabel{width: 300px; min-width: 300px; max-width: 300px;text-align:center}")
     dialog.setWindowTitle("Application Crash - Bug Report")
     dialog.setText(html_crash_text)
     dialog.setTextInteractionFlags(Qt.LinksAccessibleByMouse)
     dialog.setDetailedText(error_text)
     dialog.setStandardButtons(QMessageBox.Ok)
     dialog.setIcon(QMessageBox.Warning)
+    dialog.setWidth(800)
     dialog.exec_()
     return dialog  # for testing
 
@@ -67,7 +89,8 @@ def show_bug_dialog_exc_hook(exctype, excvalue, tb):
     traceback.print_tb(tb, limit=10)
     try:
         bug_reporting_dialog(excvalue, tb)
-    except Exception:
+    except Exception as e:
+        print(str(e))
         # gui does not work anymore - nothing to do
         sys.exit(2)
     sys.exit(1)
