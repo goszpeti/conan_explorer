0b5f95de9aacb95fd5039e53898e20705ddec195
peter-gosztolya <peter.gosztolya@gmail.com>
2022-04-12 18:24:08 +0200

    Uic jit compilation is now based on modif.-date

--- src/conan_app_launcher/ui/__init__.py
+++ src/conan_app_launcher/ui/__init__.py
@@ -10,8 +10,9 @@ for ui_file in current_dir.glob("**/*.ui"):
     py_ui_file = Path("NULL")
     try:
         py_ui_file = ui_file.parent / (ui_file.stem + "_ui.py")
-        if py_ui_file.exists() and DEBUG_LEVEL > 1:
-            Logger().debug("Converting " + str(py_ui_file))
-            os.system(f"pyuic5 -o {str(py_ui_file)} {str(ui_file)}")
+        if py_ui_file.exists() and py_ui_file.stat().st_mtime > ui_file.stat().st_mtime:
+            continue
+        Logger().debug("Converting " + str(py_ui_file))
+        os.system(f"pyuic5 -o {str(py_ui_file)} {str(ui_file)}")
     except Exception as e:
         Logger().warning(f"Can't convert {str(py_ui_file)}: {str(e)}")
