9b2029631251ab4a2a9c8e1b297ac57f7cb9b973
goszpeti <peter.gosztolya@gmail.com>
2023-03-16 21:32:21 +0100

    Make options compatible with older conan versions

--- test/testdata/conan/conanfile.py
+++ test/testdata/conan/conanfile.py
@@ -11,7 +11,7 @@ class Example(ConanFile):
     name = "example"
     #default_user = "user"
     #default_channel = "very_long_channel_name_you_should_not_do_this"
-    options = {"shared": [True, False], "fPIC": [True, False], "variant": [None, "ANY"]}
+    options = {"shared": [True, False], "fPIC": [True, False], "variant": "ANY"}
     default_options = {"shared": True, "fPIC": True, "variant": "var1"}
     settings = "os", "arch", "compiler", "build_type"
     no_copy_source = True
