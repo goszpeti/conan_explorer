b8db69d321225d544e75ff59d3cf3dfb0a8bdaf6
goszpeti <peter.gosztolya@gmail.com>
2023-04-20 20:22:50 +0200

    Revert to old install mechanism for plugin test

--- test/02_integration/test_gui_main.py
+++ test/02_integration/test_gui_main.py
@@ -250,7 +250,8 @@ def test_tabs_cleanup_on_load_config_file(base_fixture, ui_config_fixture, qtbot
 @pytest.mark.conanv1
 def test_example_plugin(app_qt_fixture, base_fixture: PathSetup):
     example_plugin_path = base_fixture.core_path / "doc/example_plugin"
-    os.system(f"{sys.executable} -m pip install -e {example_plugin_path}")
+    os.system(f"{sys.executable} -m pip install wheel") # needed for --no-use-pep517
+    os.system(f"{sys.executable} -m pip install {example_plugin_path} --no-use-pep517")
     plugin_file_path = example_plugin_path / "cal_example_plugin" / "plugin.ini"
     assert plugin_file_path.exists()
     app.active_settings._read_ini()  # reload settings
