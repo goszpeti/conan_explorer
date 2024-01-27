00c1f74cd490f7dd7f548ea9f6b8f5b368251f36
goszpeti <peter.gosztolya@gmail.com>
2024-01-27 16:18:27 +0100

    Work on diff feature

--- src/conan_explorer/ui/dialogs/pkg_diff/diff.py
+++ src/conan_explorer/ui/dialogs/pkg_diff/diff.py
@@ -17,13 +17,21 @@ class PkgDiffDialog(QDialog):
         self._ui.setupUi(self)
         self._left_content = {}
         self._right_content = {}
-        self.setWindowTitle("Diff")
+        self.setWindowTitle("Compare Packages")
         self._left_highlighter = ConfigHighlighter(
             self._ui.left_text_browser.document(), "yaml")
         self._right_highlighter = ConfigHighlighter(
             self._ui.right_text_browser.document(), "yaml")
         self._diff_highlighter = ConfigHighlighter(
             self._ui.diff_text_browser.document(), "yaml")
+        self._ui.button_box.accepted.connect(self.close)
+
+        # set up changed left element connection
+
+
+    def add_diff_item(self):
+        pass
+
 
     def set_left_content(self, content: Dict[str, Any]):
         self._left_content = content
@@ -38,12 +46,13 @@ class PkgDiffDialog(QDialog):
         self._ui.right_text_browser.setText(pkg_info)
 
     def update_diff(self):
+        # populate left diff item menu
+
+        # set left and right content with first two diff elements
         from dictdiffer import diff
         pkg_diff = list(diff(self._left_content, self._right_content))
+        # filter other id 
+        pkg_diff = list(filter(lambda diff: diff[1] != "id", pkg_diff))
+        pkg_diff = list(map(lambda diff: diff[1:], pkg_diff))
         self._ui.diff_text_browser.setText(pformat(pkg_diff).translate(
             {ord("{"): None, ord("}"): None, ord("'"): None}))
-
-        # self._ui.file_edit.setText(app.active_settings.get_string(FILE_EDITOR_EXECUTABLE))
-        # self._ui.browse_button.clicked.connect(self.on_browse_clicked)
-        # self._ui.button_box.accepted.connect(self.on_save)
-        # self._ui.button_box.rejected.connect(self.close)
