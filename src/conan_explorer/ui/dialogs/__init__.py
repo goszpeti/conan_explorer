805612986adab07fb81a7c7f7d978f57df919a66
goszpeti <peter.gosztolya@gmail.com>
2024-09-08 13:36:22 +0200

    Move more functionality to QuestionWithItemListDialog

--- src/conan_explorer/ui/dialogs/__init__.py
+++ src/conan_explorer/ui/dialogs/__init__.py
@@ -1,11 +1,22 @@
-
 # base dialogs from which other dialogs can inherit
-from .question_with_item_list import QuestionWithItemListDialog
-from .reorder import ReorderController, ReorderDialog, ReorderingModel
-
-# specific dialogs
-from .crash import show_bug_reporting_dialog
 from .conan_cache_cleanup import ConanCacheCleanupDialog
 from .conan_install import ConanInstallDialog
 from .conan_remove import ConanRemoveDialog
+
+# specific dialogs
+from .crash import show_bug_reporting_dialog
 from .file_editor_selection import FileEditorSelDialog
+from .question_with_item_list import QuestionWithItemListDialog
+from .reorder import ReorderController, ReorderDialog, ReorderingModel
+
+__all__ = [
+    "QuestionWithItemListDialog",
+    "ReorderController",
+    "ReorderDialog",
+    "ReorderingModel",
+    "show_bug_reporting_dialog",
+    "ConanCacheCleanupDialog",
+    "ConanInstallDialog",
+    "ConanRemoveDialog",
+    "FileEditorSelDialog",
+]
--- src/conan_explorer/ui/dialogs/question_with_item_list/qwil.py
+++ src/conan_explorer/ui/dialogs/question_with_item_list/qwil.py
@@ -1,14 +1,23 @@
 from typing import Optional
-from PySide6.QtWidgets import QDialog, QWidget, QStyle
+
+from PySide6.QtWidgets import QDialog, QStyle, QWidget
+
 
 class QuestionWithItemListDialog(QDialog):
-
-    def __init__(self, parent: Optional[QWidget]) -> None:
+    def __init__(
+        self,
+        parent: Optional[QWidget],
+        icon: QStyle.StandardPixmap = QStyle.StandardPixmap.SP_MessageBoxQuestion,
+    ) -> None:
         super().__init__(parent)
         from .qwil_ui import Ui_Dialog
+
         self._ui = Ui_Dialog()
         self._ui.setupUi(self)
-        pixmapi = getattr(QStyle, "SP_MessageBoxQuestion")
-        if pixmapi:
-            icon = self.style().standardIcon(pixmapi)
-            self._ui.icon.setPixmap(icon.pixmap(40,40))
+        std_icon = self.style().standardIcon(icon)
+        self._ui.icon.setPixmap(std_icon.pixmap(40, 40))
+        self.button_box = self._ui.button_box
+        self.item_list_widget = self._ui.list_widget
+
+    def set_question_text(self, text: str):
+        self._ui.question_label.setText(text)
