c3b933175dccb09410f59273d920d2ba9c5d674a
goszpeti <peter.gosztolya@gmail.com>
2024-09-22 12:37:35 +0200

    Make compile_ui_file_if_newer public again for plugins to use

--- src/conan_explorer/ui/__init__.py
+++ src/conan_explorer/ui/__init__.py
@@ -4,17 +4,18 @@ from pathlib import Path
 from conan_explorer import DEBUG_LEVEL, INVALID_PATH
 
 
+def compile_ui_file_if_newer(ui_file: Path):
+    from conan_explorer.app.logger import Logger
+
+    py_ui_file = ui_file.parent / (ui_file.stem + "_ui.py")
+    if py_ui_file.exists() and py_ui_file.stat().st_mtime > ui_file.stat().st_mtime:
+        return
+    Logger().debug("Converting " + str(py_ui_file))
+    os.system(f"pyside6-uic -o {str(py_ui_file)} {str(ui_file)}")
+
+
 # compile uic files, if needed
 def recompile_ui_files():
-    def compile_ui_file_if_newer(ui_file: Path):
-        from conan_explorer.app.logger import Logger
-
-        py_ui_file = ui_file.parent / (ui_file.stem + "_ui.py")
-        if py_ui_file.exists() and py_ui_file.stat().st_mtime > ui_file.stat().st_mtime:
-            return
-        Logger().debug("Converting " + str(py_ui_file))
-        os.system(f"pyside6-uic -o {str(py_ui_file)} {str(ui_file)}")
-
     if DEBUG_LEVEL > 0:
         current_dir = Path(__file__).parent
         for ui_file in current_dir.glob("**/*.ui"):
